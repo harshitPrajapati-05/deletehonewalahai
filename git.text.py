23c99c9adf6e266f42840928f174634edf89705d
Harshit Prajapati <harshitprajapati5114@gmail.com>
2025-09-01 19:32:34 +0530

    fibonaaci series added

---
+++ git.text.py
@@ -0,0 +1,12 @@
+#write a program to print a fibonnaci series
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
+    print()
+    return
+
+if __name__ == "__main__":
+    n = int(input("Enter the number of terms: "))
+    fibonacci(n)
